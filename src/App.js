3760b9ebd8d94240c73aac7e70a5d1d0349977ea
Firasbn1 <130716590+Firasbn1@users.noreply.github.com>
2024-10-02 18:08:50 +0100

    Added App component with layout and posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./Header.js";
+import Post from "./Post.js";
+import Side from "./Side.js";
+import Footer from "./Footer.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Header />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          style={{ display: "flex", justifyContent: "center", width: "60%" }}
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            style={{
+              width: "60%",
+            }}
+          >
+            <Post number="1" like="77" comment="20">
+              <hr style={{ width: "100%" }}></hr>
+              <img
+                style={{ width: "100%", height: "100px" }}
+                src="\R.png"
+                alt="" // Add an empty alt prop for decorative images
+              ></img>
+            </Post>
+            <Post number="2" like="2">
+              <hr style={{ width: "100%" }}></hr>
+              <img
+                style={{ width: "100%", height: "100px" }}
+                src="\R.png"
+                alt="" // Add an empty alt prop for decorative images
+              ></img>
+            </Post>
+            <Post number="3" like="85" comment="55">
+              <hr style={{ width: "100%" }}></hr>
+              <img
+                style={{ width: "100%", height: "100px" }}
+                src="\R.png"
+                alt="" // Add an empty alt prop for decorative images
+              ></img>
+            </Post>
+            <Post like="912" comment="42">
+              <hr style={{ width: "100%" }}></hr>
+              <img
+                style={{ width: "100%", height: "100px" }}
+                src="\R.png"
+                alt="" // Add an empty alt prop for decorative images
+              ></img>
+            </Post>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "40%",
+              height: "50%",
+              borderRadius: "10px",
+              boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+              margin: "50px",
+              padding: "5px",
+              border: "solid #de57de 2px",
+            }}
+          >
+            <Side />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
